5db6f47d93ae736a0bed1346d7af599127e00d25
Rand Jackson <randjackson3@gmail.com>
2020-08-30 14:59:30 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Leaflet with Angular Demo
+Learning how to use Leaflet with Angular to visualize map data
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LeafletModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
